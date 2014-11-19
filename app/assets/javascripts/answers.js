a3bd618557a46bf05a36a1d5d8a1bcea9847408d
Sarah Schachman <sarahschachman@college.harvard.edu>
2014-11-19 14:13:54 -0500

    response buttons DONE woohoo

--- app/assets/javascripts/answers.js
+++ app/assets/javascripts/answers.js
@@ -4,9 +4,12 @@ $(document).ready(function(){
 
   //if you wish to keep both the divs hidden by default then dont forget to hide //them           
   $("#answers").hide();
+  $("#response").hide();
 
-  $("#submit").click(function(){
+  $("#submit_answer").click(function(){
        $("#answers").show("slow");
+       $("#submit_answer").hide("slow");
+       $("#response").show("slow");
   });
 
       
