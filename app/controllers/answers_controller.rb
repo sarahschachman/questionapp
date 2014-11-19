cd6b2e824a06dc1fddb0da6e9a10ae30f248848f
Sarah Schachman <sarahschachman@college.harvard.edu>
2014-11-18 22:07:33 -0500

    added landing page controllers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,6 @@
 class AnswersController < ApplicationController
   include AnswersHelper
+  include QuestionsHelper
   def new
     @question = Question.find(params[:question_id])
     @answer = @question.answers.build
@@ -10,5 +11,11 @@ class AnswersController < ApplicationController
     answer = Answer.new(params.require(:answer)
       .permit(:text, :question_id))
     add_answer(answer)
+    @question_id = params[:question_id]
+    @answer = Answer.new
+    @questions = get_question_list()
+    @question = @questions[(params[:question_id]).to_i - 1]["text"]
+    @answers = @questions[(params[:question_id]).to_i - 1]["answers"]
+    render "questions/show"
   end
 end
